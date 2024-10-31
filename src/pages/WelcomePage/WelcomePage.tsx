670500db037e1fd3aba24722568374274453f3d2
yurii <robka4an@gmail.com>
2024-10-31 16:39:10 +0200

    test fetch? all good

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,7 +1,22 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 const WelcomePage = () => {
+  useEffect(() => {
+    const testApi = async () => {
+      const opportunity = await axios.get(
+        "https://help-up-ua-server.onrender.com/api/opportunities/"
+      );
+
+      return opportunity.data.data;
+    };
+
+    testApi();
+  }, []);
+
   return (
     <div className="bg-lime-400 p-7 border-2 border-cyan-700 w-64">
-      <p className="text-red-800">WelcomePage</p>
+      <ul>{}</ul>
     </div>
   );
 };
