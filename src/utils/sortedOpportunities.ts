bfda97b44f344d0f375f3474d36c1a092e0bfc9c
yurii <robka4an@gmail.com>
2024-12-31 14:20:58 +0200

    delete utils function

--- src/utils/sortedOpportunities.ts
+++
@@ -1,9 +0,0 @@
-import { IOpportunity } from "../types/opportunitiesType";
-
-export const sortedOpportunities = (opportunities: IOpportunity[]) =>
-  [...opportunities].sort((a, b) => {
-    const dateA = a.createdAt ? new Date(a.createdAt).getTime() : 0;
-    const dateB = b.createdAt ? new Date(b.createdAt).getTime() : 0;
-
-    return dateB - dateA;
-  });
